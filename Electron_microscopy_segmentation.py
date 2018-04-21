4d51c9d6e76ceb65f35ad4887ad4f355302e603e
lwkuant <xniber@gmail.com>
2018-04-21 23:22:57 +0800

    Update for Readme and model.

--- Electron_microscopy_segmentation.py
+++ Electron_microscopy_segmentation.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 import seaborn as sns
 from skimage.io import imread
+from collections import Counter
+from time import time 
 
 ### Load the data
 train_img = imread('D:/Dataset/Side_project_Electron_microscopy_3d_segmentation/training.tif')
@@ -21,27 +23,27 @@ pic = 100
 fig = plt.figure(figsize=(15, 4))
 ax_1 = fig.add_subplot(1, 3, 1)
 plt.imshow(train_img[pic], cmap='gray')
-plt.grid(None)
+plt.grid(False)
 
 ax_2 = fig.add_subplot(1, 3, 2)
 plt.hist(train_img[pic].flatten())
 
 ax_3 = fig.add_subplot(1, 3, 3)
 plt.imshow(train_img_truth[pic], cmap='gray')
-plt.grid(None)
+plt.grid(False)
 
 ### show the segmentation 
 fig = plt.figure(figsize=(12, 10))
 ax_1 = fig.add_subplot(2, 2, 1)
 plt.imshow(train_img[pic], cmap='gray')
-plt.grid(None)
+plt.grid(False)
 ax_2 = fig.add_subplot(2, 2, 2)
 plt.hist(train_img[pic].flatten())
 
 
 ax_3 = fig.add_subplot(2, 2, 3)
 plt.imshow(train_img_truth[pic]*train_img[pic], cmap='gray')
-plt.grid(None)
+plt.grid(False)
 ax_4 = fig.add_subplot(2, 2, 4)
 #plt.hist((train_img_truth[pic]*train_img[pic]).flatten())
 plt.hist((train_img[pic][train_img_truth[pic]>0]))
@@ -53,15 +55,15 @@ maximum = (train_img[pic]*[train_img_truth[pic]>0]).max()
 fig = plt.figure(figsize=(15, 4))
 ax_1 = fig.add_subplot(1, 3, 1)
 plt.imshow(train_img[pic], cmap='gray')
-plt.grid(None)
+plt.grid(False)
 
 ax_2 = fig.add_subplot(1, 3, 2)
 plt.imshow((train_img[pic]>minimum)&(train_img[pic]<maximum), cmap='gray')
-plt.grid(None)
+plt.grid(False)
 
 ax_3 = fig.add_subplot(1, 3, 3)
 plt.imshow(train_img_truth[pic]>0, cmap='gray')
-plt.grid(None)
+plt.grid(False)
 # loks not good enough...
 
 ### Evaluate the sgementation (on the benchmark segmentation)
@@ -72,6 +74,7 @@ from sklearn.metrics import roc_auc_score, roc_curve, auc
 
 fpr, tpr, th = roc_curve(y, y_pred)
 score = roc_auc_score(y, y_pred)
+score
 
 fig, ax = plt.subplots(1,1)
 ax.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.4f)' % score)
@@ -82,7 +85,6 @@ ax.set_xlabel('False Positive Rate')
 ax.set_ylabel('True Positive Rate')
 ax.set_title('ROC curve for Benchmark Segmentation')
 ax.legend(loc="lower right")
-
 
 ### Evaluate the segmentation using hand-tuned histogram
 y_pred = ((train_img[pic]>=minimum)&(train_img[pic]<=maximum))
@@ -109,7 +111,6 @@ plt.imshow(gaussian(train_img[1], sigma=4))
 ax_2 = fig.add_subplot(1, 2, 2)
 plt.imshow(train_img[1])
 
-
 ### transform the images using gaussian thresholding
 train_img = train_img/255
 
@@ -120,8 +121,7 @@ train_img = train_img/255
 ### split the data into training and test dataset
 seed = 100
 np.random.seed(seed)
-
-ind = np.random.choice(range(train_img.shape[0]), int(train_img.shape[0]*0.8),
+ind = np.random.choice(range(train_img.shape[0]), int(train_img.shape[0]*0.9),
                            replace=False)
 
 pic_x = np.copy(train_img)
@@ -139,10 +139,86 @@ pic_x_test = pic_x_test.reshape([len(pic_x_test), 768, 1024, 1])
 pic_y_test = pic_y_test.reshape([len(pic_y_test), 768, 1024, 1])
 
 ### build the model structure
+seed = 100
+np.random.seed(seed)
+import keras
+
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras import backend as K
 from keras.callbacks import TensorBoard
+
+height = 768
+width = 1024
+
+input_img = Input(shape=(768, 1024, 1))
+
+####
+#x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_img)
+#x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+#encoded = MaxPooling2D((2, 2), name='block1_pool')(x)
+
+# Block 2
+#x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+#x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+#encoded = MaxPooling2D((2, 2), name='block2_pool')(x)
+
+## Block 3
+#x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+#x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+#x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+#encoded = MaxPooling2D((2, 2), name='block3_pool')(x)
+
+# Block 4
+#x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+#x = MaxPooling2D((2, 2), name='block4_pool')(x)
+
+# Block 5
+#x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+#encoded = MaxPooling2D((2, 2), name='block5_pool')(x)
+
+
+#x = Conv2D(512, (3, 3), activation='relu', padding='same')(encoded)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#
+#x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+#x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#
+#x = Conv2D(256, (3, 3), activation='relu', padding='same')(encoded)
+#x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+#x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#
+#x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+#x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+
+#x = Conv2D(64, (3, 3), activation='relu', padding='same')(encoded)
+#x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#
+#decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+#
+#autoencoder = Model(input_img, decoded)
+#
+#autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy',  metrics=['acc'])
+#
+#autoencoder.fit(pic_x_tr, pic_y_tr,
+#                epochs=30,
+#                batch_size=1,
+#                shuffle=True,
+#                validation_data=(pic_x_test, pic_y_test),
+#                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+
+##
 
 """ Original neural structure
 input_img = Input(shape=(768, 1024, 1))
@@ -174,7 +250,7 @@ x = MaxPooling2D((2, 2), padding='same')(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-# at this moment, the shape of the image is (8, 48, 64)
+# at this moment, the shape of the image is (48, 64, 8)
 
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
@@ -186,16 +262,43 @@ x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
-autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+# 20180421: Bad
+#x = Conv2D(8, (3, 3), activation='relu', padding='same')(input_img)
+#x = MaxPooling2D((2, 2), padding='same')(x)
+#x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+#x = MaxPooling2D((2, 2), padding='same')(x)
+#x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+#x = MaxPooling2D((2, 2), padding='same')(x)
+#x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+#encoded = MaxPooling2D((2, 2), padding='same')(x)
+#
+## at this moment, the shape of the image is (48, 64, 8)
+#
+#x = Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
+#x = UpSampling2D((2, 2))(x)
+#x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+#decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
-autoencoder.fit(pic_x_tr, pic_y_tr,
-                epochs=100,
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics = ['acc']) # 2017/5/7
+# autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['acc']) # 2018/4/20, worse thant adadelta
+#autoencoder.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics = ['acc']) # 2018/4/21, worse thant adadelta
+
+start_time = time()
+autoencoder.fit(pic_x_tr, 
+                pic_y_tr,
+                epochs=50,
                 batch_size=3,
+                validation_data = [pic_x_test, pic_y_test],
                 shuffle=True,
-                validation_data=(pic_x_test, pic_y_test),
                 callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
-# it takes me around 100 mins to train the model
+print(time() - start_time)
+
 
 y_tr_pred = autoencoder.predict(pic_x_tr, batch_size=3)
 y_test_pred = autoencoder.predict(pic_x_test, batch_size=3)
@@ -248,11 +351,111 @@ plt.grid(False)
 
 ### Evaluate the model
 from sklearn.metrics import log_loss
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+## 1. Baseline
+# Accuracy
+print(np.mean([accuracy_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (1 - (pic_x_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]*1)) for i in range(pic_x_tr.shape[0])]))
+# 0.671550931157
+
+# Precision
+print(np.mean([precision_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (1 - (pic_x_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]*1)) for i in range(pic_x_tr.shape[0])]))
+# 0.129430356929
+
+# Recall
+print(np.mean([recall_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (1 - (pic_x_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]*1)) for i in range(pic_x_tr.shape[0])]))
+# 0.864888246063
+
+# F1 score
+print(np.mean([f1_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (1 - (pic_x_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]*1)) for i in range(pic_x_tr.shape[0])]))
+# 0.223435378402
+
+# ROC-AUC
+print(np.mean([roc_auc_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (1 - pic_x_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i])) for i in range(pic_x_tr.shape[0])]))
+# 0.806822370082
+
+## 2. Without tuning the threshold
+threshold = 0.5
+
+## Training
+# Accuracy
+print(np.mean([accuracy_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_tr_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_tr.shape[0])]))
+# 0.985303672584
+
+# Precision
+print(np.mean([precision_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_tr_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_tr.shape[0])]))
+# 0.909384893806
+
+# Recall
+print(np.mean([recall_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_tr_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_tr.shape[0])]))
+# 0.823150863226
+
+# F1 score
+print(np.mean([f1_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_tr_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_tr.shape[0])]))
+# 0.863054677176
+
+# ROC-AUC
+print(np.mean([roc_auc_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], y_tr_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i]) for i in range(pic_x_tr.shape[0])]))
+# 0.996151093209
+
+## Testing 
+# Accuracy
+print(np.mean([accuracy_score(pic_y_test.reshape([-1, np.prod(y_test_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_test_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.986047931746
+
+# Precision
+print(np.mean([precision_score(pic_y_test.reshape([-1, np.prod(y_test_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_test_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.899365174427
+
+# Recall
+print(np.mean([recall_score(pic_y_test.reshape([-1, np.prod(y_test_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_test_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.83250794642
+
+# F1 score
+print(np.mean([f1_score(pic_y_test.reshape([-1, np.prod(y_test_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_test_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.863340686965
+
+# ROC-AUC
+print(np.mean([roc_auc_score(pic_y_test.reshape([-1, np.prod(y_test_pred.shape[1:])])[i], y_test_pred.reshape([-1, np.prod(y_test_pred.shape[1:])])[i]) for i in range(pic_x_test.shape[0])]))
+# 0.996394102766
+
+## 3. Tuning the threshold
+
+# Aimming on the "F1 score"
+threshold_list = np.arange(0, 1, 0.05)
+
+f1_score_list = [np.mean([f1_score(pic_y_tr.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_tr_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=t)[i]) for i in range(pic_x_tr.shape[0])])\
+                         for t in threshold_list]
+
+print(threshold_list[np.argmax(f1_score_list)]) # 0.35
+print(f1_score_list[np.argmax(f1_score_list)]) # 0.8936087516718253
+
+threshold = threshold_list[np.argmax(f1_score_list)]
+
+# Accuracy
+print(np.mean([accuracy_score(pic_y_test.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.98571695066
+
+# Precision
+print(np.mean([precision_score(pic_y_test.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.854271132129
+
+# Recall
+print(np.mean([recall_score(pic_y_test.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.882002959165
+
+# F1 score
+print(np.mean([f1_score(pic_y_test.reshape([-1, np.prod(y_tr_pred.shape[1:])])[i], (y_test_pred.reshape([-1, np.prod(y_tr_pred.shape[1:])])>=threshold)[i]) for i in range(pic_x_test.shape[0])]))
+# 0.866449526991
+
+
 
 ## choose the best threshold based on the validation set on f1 score
-th_list = np.arange(0, 1, 0.05)
+# th_list = np.arange(0, 1, 0.05), 2017/5/7
+th_list = np.arange(0, 1, 0.05) # 2018/4/21
 f1_list = []
 # th_best = 0.55 (original)
 # th_best = 0.75 (2017/05/06)
@@ -307,6 +510,14 @@ with open("D:/Project/Side_project_Electron_microscopy_segmentation/model_4layer
 # serialize weights to HDF5
 autoencoder.save_weights("D:/Project/Side_project_Electron_microscopy_segmentation/model_4layers.h5")
 print("Saved model to disk")
+### 2018/04/21
+# serialize model to YAML
+model_yaml = autoencoder.to_yaml()
+with open("D:/Project/Side_project_Electron_microscopy_segmentation/model_4layers_adam.yaml", "w") as yaml_file:
+    yaml_file.write(model_yaml)
+# serialize weights to HDF5
+autoencoder.save_weights("D:/Project/Side_project_Electron_microscopy_segmentation/model_4layers_adam.h5")
+print("Saved model to disk")
 
 ### load json and create model
 from keras.models import model_from_yaml
@@ -343,27 +554,84 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 
+# use the best threshold
+
+# output from model
 a_list = []
 p_list = []
 r_list = []
 f_list = []
 for i in range(len(y_pred)):
     a_list.append(accuracy_score(test_img_truth[i].flatten(),
-                                  y_pred[i].flatten()>th_best))    
+                                  y_pred[i].flatten()>=threshold))    
     p_list.append(precision_score(test_img_truth[i].flatten(),
-                                  y_pred[i].flatten()>th_best))
+                                  y_pred[i].flatten()>=threshold))
     r_list.append(recall_score(test_img_truth[i].flatten(),
-                                  y_pred[i].flatten()>th_best))
+                                  y_pred[i].flatten()>=threshold))
     f_list.append(f1_score(test_img_truth[i].flatten(),
-                                  y_pred[i].flatten()>th_best))
+                                  y_pred[i].flatten()>=threshold))
 
 print('Results on test dataset:')
 print('Accuracy:', np.mean(a_list))
 print('Precision:', np.mean(p_list))
 print('Recall:', np.mean(r_list))
 print('F1 score:', np.mean(f_list))
+print('ROC AUC', np.mean([roc_auc_score(test_img_truth[i].flatten(), y_pred[i].flatten()) for i in range(y_pred.shape[0])]))
 
-pic = 100
+# baseline
+a_list = []
+p_list = []
+r_list = []
+f_list = []
+for i in range(len(y_pred)):
+    a_list.append(accuracy_score(test_img_truth[i].flatten(),
+                                  test_img[i].flatten()>=threshold))    
+    p_list.append(precision_score(test_img_truth[i].flatten(),
+                                  test_img[i].flatten()>=threshold))
+    r_list.append(recall_score(test_img_truth[i].flatten(),
+                                  test_img[i].flatten()>=threshold))
+    f_list.append(f1_score(test_img_truth[i].flatten(),
+                                  test_img[i].flatten()>=threshold))
+
+print('Results on test dataset:')
+print('Accuracy:', np.mean(a_list))
+print('Precision:', np.mean(p_list))
+print('Recall:', np.mean(r_list))
+print('F1 score:', np.mean(f_list))
+print('ROC AUC', np.mean([roc_auc_score(test_img_truth[i].flatten(), test_img[i].flatten()) for i in range(y_pred.shape[0])]))
+
+
+
+# use the 0.5 as threshold
+threshold = 0.5
+a_list = []
+p_list = []
+r_list = []
+f_list = []
+for i in range(len(y_pred)):
+    a_list.append(accuracy_score(test_img_truth[i].flatten(),
+                                  y_pred[i].flatten()>=threshold))    
+    p_list.append(precision_score(test_img_truth[i].flatten(),
+                                  y_pred[i].flatten()>=threshold))
+    r_list.append(recall_score(test_img_truth[i].flatten(),
+                                  y_pred[i].flatten()>=threshold))
+    f_list.append(f1_score(test_img_truth[i].flatten(),
+                                  y_pred[i].flatten()>=threshold))
+
+print('Results on test dataset:')
+print('Accuracy:', np.mean(a_list))
+print('Precision:', np.mean(p_list))
+print('Recall:', np.mean(r_list))
+print('F1 score:', np.mean(f_list))
+print('ROC AUC', np.mean([roc_auc_score(test_img_truth[i].flatten(), y_pred[i].flatten()) for i in range(y_pred.shape[0])]))
+
+
+# output a sample test data
+
+
+th_best = 0.35
+
+pic = 1
 fig = plt.figure(figsize=(16, 14))
 ax_1 = fig.add_subplot(2, 2, 1)
 plt.imshow(test_img[pic].reshape([768, 1024]), cmap='gray')
--- README.md
+++ README.md
@@ -9,21 +9,24 @@ The related information regarding this dataset can be accessed below:
 * [kaggle (the data source)](https://www.kaggle.com/kmader/electron-microscopy-3d-segmentation)
 
 ## Result
-### Structure of the model
+### 1. Structure of the model
 <img src="https://github.com/lwkuant/Side_project_Electron_microscopy_segmentation/blob/master/model_overview.jpg">
 
-### The predicted outcome on one of the example from test dataset:
+### 2. The predicted outcome on one of the example from test dataset:
 <img src="https://github.com/lwkuant/Side_project_Electron_microscopy_segmentation/blob/master/Compare_test_result_model_4layers.png">
 
-### The result looks not bad, it achieves:
-* Accuracy: 0.98
-* Precision: 0.86
-* Recall: 0.77
-* F1 score: 0.81
+### 3. Performance
+| Metrics   	| Baseline 	| CNN+Autoencoder 	|
+|-----------	|----------	|-----------------	|
+| Accuracy  	| 0.085    	| 0.98            	|
+| Precision 	| 0.05     	| 0.88            	|
+| Recall    	| 0.92     	| 0.71            	|
+| F1 score  	| 0.10     	| 0.79            	|
+| AUC       	| 0.20     	| 0.99            	|
 
 ## Conclusions and takeaways
 1. The results look not bad; however, the recall leaves some space for improvement since I think this measure is more important in this task
-2. Before building, use some computer vision-related preprocessing techniques to process the data to make it easier to find the patterns
-3. Try different structures of the model
-4. Tune more hyperparameters 
+2. Before building, use some computer vision-specific preprocessing techniques to process the data to make it easier to find the patterns
+3. treat this task as a object detection problem
+4. Tune more hyperparameters
 
